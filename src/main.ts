8e5d5ca7d4ffa293ccf22d946b759a0f665d6d1a
elviritica <e.aviles.s@gmail.com>
2024-02-17 13:25:15 +0100

    apartado obligatorios terminados

--- src/main.ts
+++ src/main.ts
@@ -73,3 +73,114 @@ const pacientes: Pacientes[] = [
     edad: 11,
   },
 ];
+
+//APARTADO 1
+const obtenPacientesAsignadosAPediatria = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  let arrayPacientesPediatria = [];
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Pediatra") {
+      arrayPacientesPediatria.push(pacientes[i]);
+    }
+  }
+  return arrayPacientesPediatria;
+};
+
+const obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios = (
+  pacientes: Pacientes[]
+): Pacientes[] => {
+  let arrayPacientesPediatriaMenoresDiez = [];
+  for (let i = 0; i < pacientes.length; i++) {
+    if (pacientes[i].especialidad === "Pediatra" && pacientes[i].edad < 10) {
+      arrayPacientesPediatriaMenoresDiez.push(pacientes[i]);
+    }
+  }
+  return arrayPacientesPediatriaMenoresDiez;
+};
+
+console.log("APARTADO 1:");
+console.log(
+  "Pacientes asignados a pediatría:",
+  obtenPacientesAsignadosAPediatria(pacientes)
+);
+console.log(
+  "Pacientes asignados a pediatría menores de 10 años",
+  obtenPacientesAsignadosAPediatriaYMenorDeDiezAnios(pacientes)
+);
+
+//APARTADO 2
+const activarProtocoloUrgencia = (pacientes: Pacientes[]): boolean => {
+  let activarProtolo = false;
+
+  for (let i = 0; i < pacientes.length; i++) {
+    if (
+      pacientes[i].frecuenciaCardiaca > 100 &&
+      pacientes[i].temperatura > 39
+    ) {
+        activarProtolo = true;
+    }
+  }
+  return activarProtolo;
+};
+
+console.log(
+  "APARTADO 2: Activar el protocolo de urgencia algún paciente tiene un ritmo cardíaco superior a 100 pulsaciones por minuto y una temperatura corporal superior a 39 grados."
+);
+console.log(
+  "Activar protocolo de seguridad?",
+  activarProtocoloUrgencia(pacientes)
+);
+
+//APARTADO 3
+const reasignaPacientesAMedicoFamilia = (pacientes: Pacientes[]): Pacientes[] => {
+    let indicePaciente = -1;
+    let pacientesReasignados = [...pacientes]; 
+  
+    for (let i = 0; i < pacientes.length; i++) {
+      if (pacientes[i].especialidad === "Pediatra") {
+        indicePaciente = i;
+        break;
+      }
+    }
+  
+    if (indicePaciente !== -1) {
+        pacientesReasignados[indicePaciente] = {
+          ...pacientesReasignados[indicePaciente],
+          especialidad: "Medico de familia",
+        };
+      }
+  
+    return pacientesReasignados;
+};
+
+console.log(
+    "APARTADO 3:"
+  );
+  console.log(
+    "Listado de pacientes tras reasignacion",
+    reasignaPacientesAMedicoFamilia(pacientes)
+  );
+
+//APARTADO 4
+const hayPacientesDePediatria = (pacientes: Pacientes[]): boolean => {
+    let tienePacientes = false;
+    for (let i = 0; i < pacientes.length; i++) {
+        if (pacientes[i].especialidad === "Pediatra") {
+            tienePacientes = true;
+            break;
+        }
+        
+    }
+    return tienePacientes;
+};
+
+console.log(
+    "APARTADO 4:"
+  );
+  console.log(
+    "Hay pacientes de pediatría?",
+    hayPacientesDePediatria(pacientes)
+  );
+
+//APARTADO 5
